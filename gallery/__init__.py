28a38bc08677102561c0975f9575562981420232
Simon Bächler <sb@feinheit.ch>
2012-08-16 18:38:52 +0200

    update setup.py

--- gallery/__init__.py
+++ gallery/__init__.py
@@ -51,17 +51,6 @@ When replacing an image file in the admin frontend, you have to save the page fo
 to update itself.
 
 """
-VERSION = (1, 0, 0, 'beta', 2)
 
-def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = "%s %s" % (version, VERSION[3])
-            if VERSION[4] != 0:
-                version = '%s %s' % (version, VERSION[4])
-    return version
+VERSION = (1, 0, 3)
+__version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
+#!/usr/bin/env python
+
+import os
 from setuptools import setup, find_packages
-from gallery import get_version
+
 setup(
-    name='feincms_gallery',
-    version=get_version(),
-    description='This is a gallery app and contenttype for Feincms.',
-    author='',
-    author_email='',
-    url='https://github.com/feinheit/feincms_gallery',
-    packages=find_packages(),
-    zip_safe=False,
+	name = 'feincms-gallery',
+	packages = find_packages(),
     include_package_data=True,
+    version=__import__('gallery').__version__,
+    description='A gallery for FeinCMS',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Simon Baechler',
+    author_email='sb@feinheit.ch',
+    url='https://github.com/feinheit/feincms_gallery',
+    license='BSD License',
+    platforms=['OS Independent'],
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Utilities',
-    ]
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
 )
