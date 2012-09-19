f0222dc1d7e72b616fc2908ef2ec9d91c85cfad9
Matthias Kestenholz <mk@spinlock.ch>
2012-09-19 12:25:45 +0200

    One warning less

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,10 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:  # Older Django versions
+    from django.conf.urls.defaults import patterns, url
+
 from gallery.admin import admin_thumbnail
+
 
 urlpatterns = patterns('',
     url(r'^thumbnail/$', admin_thumbnail, name='gallery_admin_thumbnail'),
